191ca7b6f41716d3523b40881281a53fc9dfc302
Gerd Neudert <23189582+gneu77@users.noreply.github.com>
2021-05-01 18:40:47 +0200

    version 1.0.1 : allow for multiple module instantiation / doc strings for helper functions

--- projects/rx-signals/angular-provider/CHANGELOG.md
+++ projects/rx-signals/angular-provider/CHANGELOG.md
@@ -1,3 +1,13 @@
+# 1.0.1 (2021-05-01)
+
+### Fixes
+
+- allow for multiple module instantiation
+
+### Documentation
+
+- added doc strings for helper functions
+
 # 1.0.0 (2021-04-03)
 
 - **Initial version**
--- projects/rx-signals/angular-provider/src/lib/rx-signals-store.module.ts
+++ projects/rx-signals/angular-provider/src/lib/rx-signals-store.module.ts
@@ -4,16 +4,36 @@ import { take } from 'rxjs/operators';
 
 const dummyStore = new Store();
 const storeSymbol: TypeIdentifier<Store> = { symbol: Symbol('appStore') };
+const storeEventSymbol: TypeIdentifier<Store> = { symbol: Symbol('appStoreEvent') };
 
 export type WithStoreCallback = (store: Store) => void;
 
-export const withStore = (cb: WithStoreCallback) =>
+/**
+ * This function can be used to execute code with the same store instance
+ * this is being injected by the RxSignalsStoreModule.
+ * In case this function is called in context of a testWithStore callback,
+ * the store provided by testWithStore will be used instead.
+ *
+ * @param {WithStoreCallback} cb - the callback receiving the store as argument
+ * @returns {void}
+ */
+export const withStore = (cb: WithStoreCallback): void => {
   dummyStore
     .getBehavior(storeSymbol)
     .pipe(take(1))
     .subscribe(store => cb(store));
+};
 
-export const testWithStore = (cb: WithStoreCallback) => {
+/**
+ * This function can be used to execute code with a fresh new store instance.
+ * If any code executed in context of the given callback uses the withStore function,
+ * the latter functions callback will also receive the fresh new store instance instead
+ * of the store injected by the RxSignalsStoreModule
+ *
+ * @param {WithStoreCallback} cb - the callback receiving the store as argument
+ * @returns {void}
+ */
+export const testWithStore = (cb: WithStoreCallback): void => {
   dummyStore.removeBehaviorSources(storeSymbol);
   const store = new Store();
   dummyStore.addState(storeSymbol, store);
@@ -30,6 +50,11 @@ export class RxSignalsStoreModule {
         'RxSignalsStoreModule is already loaded. Import it in a single module (e.g. CoreModule) only!',
       );
     }
-    dummyStore.addState(storeSymbol, this.store);
+    if (dummyStore.getNumberOfBehaviorSources(storeSymbol) === 0) {
+      dummyStore.addState(storeSymbol, this.store);
+      dummyStore.addReducer(storeSymbol, storeEventSymbol, store => store);
+    } else {
+      dummyStore.dispatchEvent(storeEventSymbol, this.store);
+    }
   }
 }
